eab2ef140c950df889828bdce4550a10e984e5bd
Beatrice202-ops <uzomabeatrice3@gmail.com>
2025-11-02 21:44:14 +0100

    Add UserProfile component to display user information

---
+++ alx-react-app/src/components/UserProfile.jsx
@@ -0,0 +1,14 @@
+const UserProfile = (props) => {
+     
+    return(
+        <div>
+            <h2>{props.name}</h2>
+            <p>Age: {age} </p>
+            <p>Bio {bio}</p>
+        </div>
+
+    )
+
+}
+
+export default UserProfile; 
